9a1bc1a51e80b12e83e73801bc556ddd5a4fd975
Ashokkumar Vaishnav <ashokranka30@gmail.com>
2025-02-17 23:54:33 +0530

    project lay out completed

--- src/controller/getuser.js
+++ src/controller/getuser.js
@@ -2,14 +2,12 @@ import userModel from "../models/user.js";
 const getuser = async (req, res) => {
   try {
     const user = await userModel.findOne({ _id: req.body.userId });
+    user.password = undefined;
     if (!user) {
       return res.status(401).json({ message: "user not found" });
     } else {
       res.status(200).json({
-        data: {
-          name: user.name,
-          email: user.email,
-        },
+        data: user,
       });
     }
   } catch (error) {
